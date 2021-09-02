9f76749593c1de776ad93b87e00d6952d96419e7
Peter <schmidt.multimedia@gmail.com>
2021-09-02 11:14:24 +0200

    private / Encapsulation / getter() + setter()

--- src/com/cc/java/App.java
+++ src/com/cc/java/App.java
@@ -4,6 +4,9 @@ public class App {
     public static void main(String[] args) {
       // to do
     
+      Test test = new Test("Grizabella", 29);
+      ausgabe("Namen: " + test.getName());
+      ausgabe("Age: " + test.getAge());
 
     }
 
